7d00de3fa4ced71d7ac8300cdb08708a3f630011
daniesnata <daniesnata@gmail.com>
2021-02-26 06:38:53 +0700

    change structure: no need init param and add struct chunk

--- include/blake/blake3.h
+++ include/blake/blake3.h
@@ -6,54 +6,50 @@
 enum nc_blake3_constant
 {
     NC_BLAKE3_BLOCK_BYTES    = 64,
+    NC_BLAKE3_CHUNK_BYTES    = 1024,
     NC_BLAKE3_DIGEST_BYTES   = 32, 
     NC_BLAKE3_KEY_BYTES      = 32, 
     NC_BLAKE3_SALT_BYTES     = 8, 
-    NC_BLAKE3_PERSONAL_BYTES = 8
+    NC_BLAKE3_PERSONAL_BYTES = 8,
+    NC_BLAKE3_MAX_DEPTH      = 54
 };
 
 
+#define BIT(x) (1 << (x))
 enum nc_blake3_domain_flag
 {
-    NC_BLAKE3_CHUNK_START           = (1 << 0), 
-    NC_BLAKE3_CHUNK_END             = (1 << 1), 
-    NC_BLAKE3_PARENT                = (1 << 2), 
-    NC_BLAKE3_ROOT                  = (1 << 3), 
-    NC_BLAKE3_KEYED_HASH            = (1 << 4), 
-    NC_BLAKE3_DERIVE_KEY_CONTEXT    = (1 << 5), 
-    NC_BLAKE3_DERIVE_KEY_MATERIAL   = (1 << 6)
+    NC_BLAKE3_CHUNK_START           = BIT(0), 
+    NC_BLAKE3_CHUNK_END             = BIT(1), 
+    NC_BLAKE3_PARENT                = BIT(2), 
+    NC_BLAKE3_ROOT                  = BIT(3), 
+    NC_BLAKE3_KEYED_HASH            = BIT(4), 
+    NC_BLAKE3_DERIVE_KEY_CONTEXT    = BIT(5), 
+    NC_BLAKE3_DERIVE_KEY_MATERIAL   = BIT(6)
+};
+#undef BIT
+
+struct nc_blake3_chunk 
+{
+    uint32_t h[8];
+    uint32_t chunk_counter;
+    uint8_t  buf[NC_BLAKE3_BLOCK_BYTES];
+    uint32_t buflen;
+    uint8_t  blocks_compressed;
+    uint8_t  flags;
 };
 
-typedef struct nc_blake3_state
+struct nc_blake3_state
 {
-    uint8_t  h[8];
-    uint32_t t[2];
-    uint32_t b;
-    uint32_t d;
-}
-nc_blake3_state_t;
-
-typedef struct nc_blake3_param
-{
-    uint8_t  digest_length;
-    uint8_t  key_length;
-    uint8_t  fanout;
-    uint8_t  depth;
-    uint32_t leaf_length;
-    uint8_t  node_offset[6];
-    uint8_t  node_depth;
-    uint8_t  inner_length;
-    uint8_t  salt[8];
-    uint8_t  personal[8]; 
-}
-nc_blake3_param_t;
-
+    uint32_t key[8];
+    struct nc_blake3_chunk chunk;
+    uint8_t  cv_stacklen;
+    uint8_t  cv_stack[NC_BLAKE3_MAX_DEPTH * NC_BLAKE3_DIGEST_BYTES];
+};
 
 
 /* phase API */
-int nc_blake3_init (nc_blake3_state_t *s, size_t digestlen);
-int nc_blake3_init_param (nc_blake3_state_t *s, nc_blake3_param_t *p);
-int nc_blake3_update();
+int nc_blake3_init (struct nc_blake3_state *s, size_t digestlen);
+int nc_blake3_update(struct nc_blake3_state *s, const char *in, const size_t len);
 int nc_blake3_final();
 
 /* simple API */
--- src/blake3.c
+++ src/blake3.c
@@ -11,61 +11,75 @@ static const uint32_t blake3_iv[] =
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
 };
 
+static const uint8_t blake3_message_schedule[7][16] =
+{
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+    {2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8},
+    {3, 4, 10, 12, 13, 2, 7, 14, 6, 5, 9, 0, 11, 15, 8, 1},
+    {10, 7, 12, 9, 14, 3, 13, 15, 4, 0, 11, 2, 5, 8, 1, 6},
+    {12, 13, 9, 11, 15, 10, 14, 8, 7, 2, 5, 3, 0, 1, 6, 4},
+    {9, 14, 11, 5, 8, 12, 15, 1, 13, 3, 0, 10, 2, 6, 4, 7},
+    {11, 15, 5, 0, 1, 9, 8, 6, 14, 10, 2, 12, 3, 4, 7, 13},
+};
 
-static inline get_uint32 (const void *src)
+
+static inline uint32_t get_uint32 (const void *src)
 {
     return *((uint32_t *)src);
 }
 
-static inline int blake3_init0 (nc_blake3_state_t *s)
+static inline void blake3_init_chunks (struct nc_blake3_state *s)
 {
-    memset(s, 0, sizeof(nc_blake3_state_t));
-
-    int i;
-    for (i = 0; i < sizeof(blake3_iv)/sizeof(uint32_t); ++i) {
-        s->h[i] = blake3_iv[i];
-    }
-    
-
-    return 0;
+    // TODO
 }
 
-int nc_blake3_init (nc_blake3_state_t *s, size_t digestlen)
+static inline void blake3_init_base (
+            struct nc_blake3_state *s, const uint32_t key[8], const uint8_t flags)
 {
-    nc_blake3_param_t p[1];
+    memset(s, 0, sizeof(struct nc_blake3_state));
 
+    memcpy(s->key, key, NC_BLAKE3_KEY_BYTES);
+    blake3_init_chunks(s);
+    
+    s->cv_stacklen=0;
+}
+
+static inline void blake3_compress(
+            struct nc_blake3_state *s, const uint8_t block[NC_BLAKE3_BLOCK_BYTES])
+{
+    uint32_t m[16];
+    uint32_t v[16];
+
+    size_t i;
+
+    // TODO
+    //for (i = 0; i < 16; ++i)
+    //    m[i] = get_uint32( block + i * sizeof(m[i]) );
+
+    //for (i = 0; i < 8; ++i) 
+    //    v[i] = s->h[i];
+    
+}
+
+int nc_blake3_init (struct nc_blake3_state *s, size_t digestlen)
+{
     /* bad func args */
     if ( !s || digestlen == 0 || digestlen > NC_BLAKE3_DIGEST_BYTES )
         return -1;
 
-    memset(p, 0, sizeof(p));
+    blake3_init_base(s, blake3_iv, 0);
 
-    p->digest_length = digestlen;
-    p->fanout        = 2;           /* binary tree structure */
-    p->depth         = 255;         /* unlimited tree depth */
-    p->leaf_length   = 1024;        /* chunks w/ 1024 bytes */
+    // TODO
 
-    return nc_blake3_init_param(s, p); 
+    return 0; 
 }
 
-int nc_blake3_init_param (nc_blake3_state_t *s, nc_blake3_param_t *p)
+int nc_blake3_update(struct nc_blake3_state *s, const char *in, const size_t len)
 {
-    /* bad func args */
-    if ( !s || !p ) return -1;
-
-    int i;
-    uint8_t *pp;
-
-    blake3_init0(s);
-
-    pp = (uint8_t *) p;
-    for (i = 0; i < 8; ++i) {
-        s->h[i] = get_uint32( pp + (sizeof(s->h[i]) * i) );
-    }
+    // TODO
 
     return 0;
 }
-
 
 void test_print_iv (void)
 {
